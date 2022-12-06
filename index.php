d58dc6c92eec00d5b4b5a793420d0466d4a2644c
Elcio Fagundes Boin <elcio.fagundesboin@gmail.com>
2022-12-05 21:16:59 -0300

    Finalmente decodificado json

--- index.php
+++ index.php
@@ -12,31 +12,14 @@
     <?php
         $json = file_get_contents("metadado.json");
         $data = json_decode($json);
-        print_r($data);
-        echo "<hr>";
-        var_dump($data);
-        echo "<hr>";
-        echo $data->INITIAL->id[0];
-        echo "<br>";
-        echo $data->INITIAL->A[0];
-        echo "<br>";
-        echo $data->INITIAL->B[0];
-        echo "<br><br>";
-        echo $data->INITIAL->id[1];
-        echo "<br>";
-        echo $data->INITIAL->A[1];
-        echo "<br>";
-        echo $data->INITIAL->B[1];
-        echo "<hr>";
-        foreach ($data->INITIAL as $key){
-            echo $key[0];
-            echo "<br>";
-        }
-        echo "<hr>";
-        foreach ($data->INITIAL as $key){
-            foreach($key as $n){
-                echo $n."<br>";
-            }
+
+        $id = $data->INITIAL->id;
+        $A = $data->INITIAL->A;
+        $B = $data->INITIAL->B;
+        $i = 0;
+        foreach($id as $key){
+            echo "ID = ".$id[$i]." | A=".$A[$i]." | B=".$B[$i]."<br>";
+            $i++;
         }
     ?>
 </body>
